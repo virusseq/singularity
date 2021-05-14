7348214f8f4156ac082519fa29fac608f118b91b
Alex Lepsa <lepsalex@gmail.com>
2021-05-14 10:12:26 -0400

    spring init

---
+++ src/test/java/org/cancogenvirusseq/all/AllApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cancogenvirusseq.all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
