24c61473aa31e4c2dfc3e968edbd5573986904a4
Alex Lepsa <lepsalex@gmail.com>
2021-08-11 16:07:09 -0400

    removed unused config

--- src/main/java/org/cancogenvirusseq/singularity/config/s3Client/S3ClientProperties.java
+++ src/main/java/org/cancogenvirusseq/singularity/config/s3Client/S3ClientProperties.java
@@ -28,16 +28,8 @@ import software.amazon.awssdk.regions.Region;
 public class S3ClientProperties {
   private Region region = Region.US_EAST_1;
   private URI endpoint = null;
-
   private String accessKeyId;
   private String secretAccessKey;
-
-  // Bucket name we'll be using as our backend storage
   private String bucket;
   private String dataDir;
-
-  // AWS S3 requires that file parts must have at least 5MB, except
-  // for the last part. This may change for other S3-compatible services, so let't
-  // define a configuration property for that
-  private int multipartMinPartSize = 5 * 1024 * 1024;
 }
