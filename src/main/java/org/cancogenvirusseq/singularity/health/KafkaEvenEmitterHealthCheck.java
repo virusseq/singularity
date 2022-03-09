70b3f80404ce8444b1fa3e6cf78a61809983b231
jaserud <udjaser@gmail.com>
2022-03-09 14:01:02 -0500

    add proxy sink for kafka emitter

---
+++ src/main/java/org/cancogenvirusseq/singularity/health/KafkaEvenEmitterHealthCheck.java
@@ -0,0 +1,24 @@
+package org.cancogenvirusseq.singularity.health;
+
+import lombok.RequiredArgsConstructor;
+import org.cancogenvirusseq.singularity.components.events.KafkaEventEmitter;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("kafka")
+@RequiredArgsConstructor
+public class KafkaEvenEmitterHealthCheck implements HealthIndicator {
+  private static final String MESSAGE_KEY = "kafkaConsumerDisposable";
+  private final KafkaEventEmitter kafkaEventEmitter;
+
+  @Override
+  public Health health() {
+    if (kafkaEventEmitter.getKafkaConsumerDisposable().isDisposed()) {
+      return Health.down().withDetail(MESSAGE_KEY, "disposable has stopped.").build();
+    }
+    return Health.up().withDetail(MESSAGE_KEY, "disposable is running.").build();
+  }
+}
