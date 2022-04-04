ce8767fcfa22e3d3665f7a141762cdf85314212c
jaserud <udjaser@gmail.com>
2022-04-04 15:16:33 -0400

    update spring-boot to 2.6.6 (#34)
    * update spring-boot to 2.6.6
    
    * fix TimeValue not found

--- src/main/java/org/cancogenvirusseq/singularity/components/base/ElasticSearchScroll.java
+++ src/main/java/org/cancogenvirusseq/singularity/components/base/ElasticSearchScroll.java
@@ -10,7 +10,7 @@ import org.cancogenvirusseq.singularity.components.model.AnalysisDocument;
 import org.cancogenvirusseq.singularity.config.elasticsearch.ElasticsearchProperties;
 import org.cancogenvirusseq.singularity.config.elasticsearch.ReactiveElasticSearchClientConfig;
 import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
