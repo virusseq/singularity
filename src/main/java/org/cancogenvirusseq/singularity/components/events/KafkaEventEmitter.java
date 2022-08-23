d9d41551051f4a9017e0195eaaa2e01189763005
UmmulkiramR <urangwala@oicr.on.ca>
2022-08-23 08:44:43 -0400

    #38- changed the final event delay from 1 min to 5 mins.

--- src/main/java/org/cancogenvirusseq/singularity/components/events/KafkaEventEmitter.java
+++ src/main/java/org/cancogenvirusseq/singularity/components/events/KafkaEventEmitter.java
@@ -42,7 +42,7 @@ public class KafkaEventEmitter implements EventEmitter<Instant> {
   private final KafkaConsumerConfig kafkaConsumerConfig;
 
   @Value("${files.finalEventCheckSeconds}")
-  private final Integer finalEventCheckSeconds = 60; // default to 1 minute
+  private final Integer finalEventCheckSeconds = 300; // default to 1 minute
 
   private static final AtomicReference<Instant> lastEvent = new AtomicReference<>();
 
