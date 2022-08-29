6464b14e045812ad677874ae0033493ae6875132
UmmulkiramR <urangwala@oicr.on.ca>
2022-08-29 11:54:55 -0400

    refactored code added for testing- #292.

--- src/main/java/org/cancogenvirusseq/singularity/components/model/AllArchiveHashInfo.java
+++ src/main/java/org/cancogenvirusseq/singularity/components/model/AllArchiveHashInfo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.checkerframework.checker.units.qual.A;
 
 @Getter
 @RequiredArgsConstructor
