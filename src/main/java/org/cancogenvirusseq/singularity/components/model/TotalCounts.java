6f11624386cd3b9d21007290b9e2c28bce498732
Leonardo Rivera <lrivera@oicr.on.ca>
2022-09-07 10:07:34 -0400

    issue#51 Store totalcounts in database rather than server memory

--- src/main/java/org/cancogenvirusseq/singularity/components/model/TotalCounts.java
+++ src/main/java/org/cancogenvirusseq/singularity/components/model/TotalCounts.java
@@ -1,11 +1,21 @@
 package org.cancogenvirusseq.singularity.components.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import java.util.UUID;
 
 @Value
+@Data
 @Builder
+@Table("total_count")
 public class TotalCounts {
+  @Id
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  UUID id;
   Long files;
   Long samples;
   Integer studies;
