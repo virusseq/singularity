0ca8133eae48079ef1c75ab5bf06ff73db74e854
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2022-11-17 13:56:11 -0500

    add updated_at and missing lineage analysis columns (#73)
    * add updated_at and missing lineage analysis columns
    
    * set last updated at
    
    Co-authored-by: Leonardo Rivera <lrivera@oicr.on.ca>

--- src/main/java/org/cancogenvirusseq/singularity/components/utils/TsvUtils.java
+++ src/main/java/org/cancogenvirusseq/singularity/components/utils/TsvUtils.java
@@ -18,19 +18,19 @@
 
 package org.cancogenvirusseq.singularity.components.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import org.cancogenvirusseq.singularity.components.model.AnalysisDocument;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Nullable;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import javax.annotation.Nullable;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import lombok.Getter;
-import org.cancogenvirusseq.singularity.components.model.AnalysisDocument;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TsvUtils {
@@ -44,80 +44,91 @@ public class TsvUtils {
 
   @Getter
   private static final byte[] header =
-      (String.join(
-                  "\t",
-                  List.of(
-                      "study_id",
-                      "specimen collector sample ID",
-                      "sample collected by",
-                      "sequence submitted by",
-                      "submission date",
-                      "sample collection date",
-                      "sample collection date null reason",
-                      "geo_loc_name (country)",
-                      "geo_loc_name (state/province/territory)",
-                      "organism",
-                      "isolate",
-                      "fasta header name",
-                      "purpose of sampling",
-                      "purpose of sampling details",
-                      "anatomical material",
-                      "anatomical part",
-                      "body product",
-                      "environmental material",
-                      "environmental site",
-                      "collection device",
-                      "collection method",
-                      "host (scientific name)",
-                      "host disease",
-                      "host age",
-                      "host age null reason",
-                      "host age unit",
-                      "host age bin",
-                      "host gender",
-                      "purpose of sequencing",
-                      "purpose of sequencing details",
-                      "sequencing instrument",
-                      "sequencing protocol",
-                      "raw sequence data processing method",
-                      "dehosting method",
-                      "consensus sequence software name",
-                      "consensus sequence software version",
-                      "breadth of coverage value",
-                      "depth of coverage value",
-                      "reference genome accession",
-                      "bioinformatics protocol",
-                      "gene name",
-                      "diagnostic pcr Ct value",
-                      "diagnostic pcr Ct value null reason",
-                      "GISAID accession",
-                      "lineage name",
-                      "lineage analysis software name",
-                      "lineage analysis software version"))
-              + "\n")
-          .getBytes(StandardCharsets.UTF_8);
+    (String.join(
+      "\t",
+      List.of(
+        "study_id",
+        "specimen collector sample ID",
+        "lineage name",
+        "lineage analysis software name",
+        "lineage analysis software version",
+        "lineage analysis software data version",
+        "scorpio call",
+        "scorpio version",
+        "sample collected by",
+        "sequence submitted by",
+        "submission date",
+        "sample collection date",
+        "sample collection date null reason",
+        "geo_loc_name (country)",
+        "geo_loc_name (state/province/territory)",
+        "organism",
+        "isolate",
+        "fasta header name",
+        "purpose of sampling",
+        "purpose of sampling details",
+        "anatomical material",
+        "anatomical part",
+        "body product",
+        "environmental material",
+        "environmental site",
+        "collection device",
+        "collection method",
+        "host (scientific name)",
+        "host disease",
+        "host age",
+        "host age null reason",
+        "host age unit",
+        "host age bin",
+        "host gender",
+        "purpose of sequencing",
+        "purpose of sequencing details",
+        "sequencing instrument",
+        "sequencing protocol",
+        "raw sequence data processing method",
+        "dehosting method",
+        "consensus sequence software name",
+        "consensus sequence software version",
+        "breadth of coverage value",
+        "depth of coverage value",
+        "reference genome accession",
+        "bioinformatics protocol",
+        "gene name",
+        "diagnostic pcr Ct value",
+        "diagnostic pcr Ct value null reason",
+        "GISAID accession",
+        "last updated at"
+      ))
+      + "\n")
+      .getBytes(StandardCharsets.UTF_8);
 
   public static byte[] analysisDocumentsToTsvRowsBytes(List<AnalysisDocument> analysisDocuments) {
     return (analysisDocuments.stream()
-                .map(TsvUtils::analysisDocumentToTsvRow)
-                .collect(Collectors.joining("\n"))
-            + // join rows with newline
-            "\n" // append newline to final batch of rows
-        )
-        .getBytes(StandardCharsets.UTF_8);
+      .map(TsvUtils::analysisDocumentToTsvRow)
+      .collect(Collectors.joining("\n"))
+      + // join rows with newline
+      "\n" // append newline to final batch of rows
+    )
+      .getBytes(StandardCharsets.UTF_8);
   }
 
   public static byte[] analysisDocumentToTsvRowBytes(AnalysisDocument analysisDocument) {
     return (analysisDocumentToTsvRow(analysisDocument)
-            + "\n" // append newline to final batch of rows
-        )
-        .getBytes(StandardCharsets.UTF_8);
+      + "\n" // append newline to final batch of rows
+    )
+      .getBytes(StandardCharsets.UTF_8);
   }
 
   private static String analysisDocumentToTsvRow(AnalysisDocument analysisDocument) {
     return stringsToTsvRow(jsonNodeToString(
         analysisDocument.getStudyId(),
         analysisDocument.getDonors().get(0).getSubmitterDonorId(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getLineageName(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getLineageAnalysisSoftwareName(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getLineageAnalysisSoftwareVersion(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getLineageAnalysisSoftwareDataVersion(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getScorpioCall(),
+        analysisDocument.getAnalysis().getLineageAnalysis().getScorpioVersion(),
         analysisDocument.getAnalysis().getSampleCollection().getSampleCollectedBy(),
         analysisDocument.getAnalysis().getSampleCollection().getSequenceSubmittedBy(),
         analysisDocument.getAnalysis().getFirstPublishedAt(),
@@ -159,20 +170,20 @@ public class TsvUtils {
         analysisDocument.getAnalysis().getPathogenDiagnosticTesting().getGeneName(),
         analysisDocument.getAnalysis().getPathogenDiagnosticTesting().getDiagnosticPcrCtValue(),
         analysisDocument
-            .getAnalysis()
-            .getPathogenDiagnosticTesting()
-            .getDiagnosticPcrCtValueNullReason(),
+          .getAnalysis()
+          .getPathogenDiagnosticTesting()
+          .getDiagnosticPcrCtValueNullReason(),
         analysisDocument.getAnalysis().getDatabaseIdentifiers().getGisaidAccession(),
-        analysisDocument.getAnalysis().getLineageAnalysis().getLineageName(),
-        analysisDocument.getAnalysis().getLineageAnalysis().getLineageAnalysisSoftwareName(),
-        analysisDocument.getAnalysis().getLineageAnalysis().getLineageAnalysisSoftwareVersion()));
+        analysisDocument.getAnalysis().getLastUpdatedAt()
+      )
+    );
   }
 
   private static String[] jsonNodeToString(JsonNode... jsonNodeList) {
     return Arrays.stream(jsonNodeList).map(jsonNode -> {
       if (jsonNode == null) {
         return "";
-      }else if (jsonNode.isArray()) {
+      } else if (jsonNode.isArray()) {
         List<String> newList = new ArrayList<>();
         jsonNode.iterator().forEachRemaining(e -> newList.add(e.textValue()));
         return String.join(LIST_SEPARATOR, newList);
@@ -184,18 +195,18 @@ public class TsvUtils {
 
   private static String stringsToTsvRow(String... strings) {
     return Optional.of(
-            Arrays.stream(strings)
-                .reduce(
-                    new StringBuilder(),
-                    (acc, curr) -> {
-                      acc.append(valueIfPresentOrEmpty(curr));
-                      acc.append("\t");
-                      return acc;
-                    },
-                    StringBuilder::append)
-                .toString())
-        .map(row -> row.substring(0, row.length() - 1)) // trim trailing "\t"
-        .orElse("");
+        Arrays.stream(strings)
+          .reduce(
+            new StringBuilder(),
+            (acc, curr) -> {
+              acc.append(valueIfPresentOrEmpty(curr));
+              acc.append("\t");
+              return acc;
+            },
+            StringBuilder::append)
+          .toString())
+      .map(row -> row.substring(0, row.length() - 1)) // trim trailing "\t"
+      .orElse("");
   }
 
   private static String valueIfPresentOrEmpty(@Nullable String s) {
